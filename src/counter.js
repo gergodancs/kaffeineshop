95c63084198bd13d42566234a7532bb5deee1c12
gergely dancs <dancs.gergelyy@gmail.com>
2024-12-11 19:59:43 +0100

    cloudflaretest2

--- src/counter.js
+++ src/counter.js
@@ -2,7 +2,7 @@ export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
-    element.innerHTML = `a szamlalalo ${counter}`
+    element.innerHTML = `a szamlalo ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
